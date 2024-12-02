9d64b03cbc725537bf865e8a661b128f54299196
Atte Moisio <attemoisio@attemoisio.fi>
2024-12-02 11:07:35 +0200

    Create skeleton for day 3

---
+++ day03/README.md
@@ -0,0 +1,3 @@
+# <TITLE OF ASSIGNMENT>
+
+Link to assignment: [https://adventofcode.com/2024/day/3]
---
+++ day03/day03.go
@@ -0,0 +1,11 @@
+package day03
+
+func SolvePart1(input string) int {
+	// Implement part 1 logic here
+	return 0
+}
+
+func SolvePart2(input string) int {
+	// Implement part 2 logic here
+	return 0
+}
