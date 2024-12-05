9125543437a03b096909529a1cd9d308b414964a
Atte Moisio <attemoisio@attemoisio.fi>
2024-12-05 12:12:47 +0200

    Solve day 5

--- day05/day05.go
+++ day05/day05.go
@@ -1,26 +1,98 @@
-package day00
+package day05
 
 import (
+	"aoc2024/utils"
+	"slices"
 	"strconv"
 	"strings"
 )
 
-func SolvePart1(input string) int {
-	inputLines := strings.Split(input, "\n")
-	return sum(inputLines)
+type Rule struct {
+	X int
+	Y int
 }
 
-func sum(values []string) int {
+func SolvePart1(input string) int {
+	rules, pages := parseInput(input)
+	filteredPages := pagesInRightOrder(pages, rules)
+	return sumOfMiddlePageNumbers(filteredPages)
+}
+
+func SolvePart2(input string) int {
+	rules, pages := parseInput(input)
+	filteredPages := pagesInWrongOrder(pages, rules)
+	orderPages(filteredPages, rules)
+	return sumOfMiddlePageNumbers(filteredPages)
+}
+
+func orderPages(pages [][]int, rules []Rule) [][]int {
+	for _, page := range pages {
+		slices.SortFunc(page, ruleComparator(rules))
+	}
+	return pages
+}
+
+func sumOfMiddlePageNumbers(pages [][]int) int {
 	sum := 0
-	for _, value := range values {
-		if number, err := strconv.Atoi(value); err == nil {
-			sum += number
-		}
+	for _, page := range pages {
+		middlePageNum := page[len(page)/2]
+		sum += middlePageNum
 	}
 	return sum
 }
 
-func SolvePart2(input string) int {
-	// Implement part 2 logic here
-	return 0
+func pagesInRightOrder(pages [][]int, rules []Rule) [][]int {
+	filtered := [][]int{}
+	for _, page := range pages {
+		if slices.IsSortedFunc(page, ruleComparator(rules)) {
+			filtered = append(filtered, page)
+		}
+	}
+	return filtered
+}
+
+func pagesInWrongOrder(pages [][]int, rules []Rule) [][]int {
+	filtered := [][]int{}
+	for _, page := range pages {
+		if !slices.IsSortedFunc(page, ruleComparator(rules)) {
+			filtered = append(filtered, page)
+		}
+	}
+	return filtered
+}
+
+func ruleComparator(rules []Rule) func(a, b int) int {
+	return func(a, b int) int {
+		if slices.Contains(rules, Rule{X: a, Y: b}) {
+			return -1
+		}
+		return 0
+	}
+}
+
+func parseInput(input string) ([]Rule, [][]int) {
+	parts := strings.SplitN(input, "\n\n", 2)
+	return parseOrderingRules(parts[0]), parseUpdateNumbers(parts[1])
+}
+
+func parseOrderingRules(input string) []Rule {
+	rules := []Rule{}
+	for _, line := range strings.Split(input, "\n") {
+		separatorIndex := strings.Index(line, "|")
+		// Trust the input, ignore errors
+		x, _ := strconv.Atoi(line[:separatorIndex])
+		y, _ := strconv.Atoi(line[separatorIndex+1:])
+		rules = append(rules, Rule{X: x, Y: y})
+	}
+	return rules
+}
+
+func parseUpdateNumbers(input string) [][]int {
+	updateNumbers := [][]int{}
+	for _, line := range strings.Split(input, "\n") {
+		// Trust the input, ignore errors
+		numbers, _ := utils.ToInts(strings.Split(line, ","))
+		updateNumbers = append(updateNumbers, numbers)
+	}
+	return updateNumbers
 }
