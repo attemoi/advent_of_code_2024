e2fd385a251ae9d6f7e5784462d8832c08089282
Atte Moisio <attemoisio@attemoisio.fi>
2024-12-11 22:55:41 +0200

    Add skeleton for day 12

---
+++ day12/README.md
@@ -0,0 +1,5 @@
+# Day 12: TBD
+
+This is a template to be copy-pasted for each day
+
+Link to assignment: [https://adventofcode.com/2024/day/12]
---
+++ day12/day12_test.go
@@ -0,0 +1,15 @@
+package day12
+
+import (
+	"aoc2024/utils"
+	"testing"
+)
+
+func TestSolvePart1(t *testing.T) {
+	input := utils.ReadInput("input_test.txt")
+	expected := 60
+	result := SolvePart1(string(input))
+	if result != expected {
+		t.Errorf("Expected %d, got %d", expected, result)
+	}
+}
